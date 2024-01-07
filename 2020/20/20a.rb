62c701e461f55a4d1c2902b9334ef6be7e8fefde
thofoer <thofoer@gmx.de>
2024-01-07 19:51:37 +0100

    aoc 2020

---
+++ 2020/20/20a.rb
@@ -0,0 +1,29 @@
+def toNumber(s)
+    s.gsub(?., ?0).gsub(?#, ?1).to_i(2)
+end
+
+tiles = File.read("input.txt").split("\n\n").map do |p|
+    name, *tile = p.split("\n")
+    number = name.scan(/\d+/).flatten.first.to_i
+
+    top = tile.first
+    buttom = tile.last
+    left  = tile.map{ |z| z[0]  }.join
+    right = tile.map{ |z| z[-1] }.join
+
+    res = Set.new
+    [top, buttom, left, right].each do |s|
+        res << toNumber(s) << toNumber(s.reverse)        
+    end
+    [res, number]
+end.to_h
+
+
+all = Set[* tiles.keys]
+
+adj = tiles.map{|k,v|    
+    others =  (all - Set[k] ).reduce(&:+)
+   [ (others & k).size / 2, v]
+}
+
+print adj.select{|c,_| c==2}.map(&:last).reduce(&:*)
