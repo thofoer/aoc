0640ef4ae32cebb464f46e54913e0a80a7e82bcb
thofoer <thofoer@gmx.de>
2024-12-04 20:35:57 +0100

    aoc 2024 Tag 4

--- 2024/04/04a.rb
+++ 2024/04/04a.rb
@@ -1,11 +1,13 @@
 input = File.read("input.txt").split("\n")
 width = input.size
 height = input[0].size
-coords = {}
+grid = {}
+coords = []
 
 width.times do |y|
   height.times do |x|
-    coords[Complex(x,y)] = input[y][x] 
+    grid[Complex(x,y)] = input[y][x] 
+    coords << Complex(x,y)
   end
 end
 
@@ -13,7 +15,7 @@ dir = [0+1i, 0-1i, 1+0i, -1+0i, 1+1i, 1-1i, -1+1i, -1-1i]
 
 pattern = dir.map{ |d| (0..3).to_a.map{|e| e * d}}
   
-p (0..width).to_a.product((0..height).to_a)
-   .map{ |x,y| pattern.map{ |p| p.map{|q| Complex(x, y) + q}}   }
-   .sum{ |a| a.count{ |z| z.map{ |c| coords[c]} == %w{X M A S}}}
+p coords
+   .map{ |c| pattern.map{ |p| p.map{|q| c + q}}   }
+   .sum{ |a| a.count{ |z| z.map{ |c| grid[c]} == %w{X M A S}}}
   
