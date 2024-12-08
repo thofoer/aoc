bd6326f542d943e7028e5a5ec4b51130c601f667
thofoer <thofoer@gmx.de>
2024-12-08 14:31:18 +0100

    aoc 2024 Tag 8

--- 2024/08/08b.rb
+++ 2024/08/08b.rb
@@ -1,6 +1,5 @@
 input = File.read("input.txt").split("\n")
-@width = input.size
-@height = input[0].size
+@width, @height = input.size, input[0].size
 antennas = Hash.new{ |h,k| h[k] = [] }
 
 @width.times do |y|
@@ -9,24 +8,21 @@ antennas = Hash.new{ |h,k| h[k] = [] }
   end
 end
 
-def inbounds(c)
-  c.real.between?(0,@width-1) && c.imag.between?(0,@height-1)
-end
+def inbounds?(c) = c.real.between?(0, @width-1) && c.imag.between?(0, @height-1) 
 
-antinodes1 = Set.new
-antinodes2 = Set.new
+antinodes1, antinodes2 = Set.new, Set.new
 
 antennas.values.each do |a|
     a.combination(2) do |x,y|       
        diff = x-y
        f = 0
-       while inbounds(pos = x + f*diff) do
+       while inbounds?(pos = x + f*diff) do
          antinodes1 << pos if f==1
          antinodes2 << pos
          f+=1
        end
        f = 0
-       while inbounds(pos = y - f*diff) do
+       while inbounds?(pos = y - f*diff) do
          antinodes1 << pos if f==1
          antinodes2 << pos
          f+=1
@@ -34,6 +30,4 @@ antennas.values.each do |a|
     end
 end
 
-puts "Part 1: #{antinodes1.count}"
-puts "Part 2: #{antinodes2.count}"
-
+puts "Part 1: #{antinodes1.count}", "Part 2: #{antinodes2.count}"
