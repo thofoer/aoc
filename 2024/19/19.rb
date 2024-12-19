5c0f4c5eaa684f9dd1079b2be4b5e2ef9918dfb9
thofoer <thofoer@gmx.de>
2024-12-19 14:31:42 +0100

    aoc 2024 Tag 19

--- 2024/19/19.rb
+++ 2024/19/19.rb
@@ -1,21 +1,12 @@
 t, p = File.read("input.txt").split("\n\n")
 
+@cache = {}
 @towels = t.scan(/([wubgr]+)/).flatten
 patterns = p.split("\n")
 
-def count(pattern)
+def count(pattern)  =
+    @cache[pattern] ||= @towels.filter{ pattern.start_with?(_1)}.sum { |t|  t == pattern ? 1 :  count(pattern[t.size..]) }
 
-    def countIter(pattern)    
-        return @h[pattern] if @h[pattern]
-        count = 0
-
-        @t.filter{ pattern.start_with?(_1)}.each { |t| count +=  t == pattern ? 1 :  countIter(pattern[t.size..]) }
-        @h[pattern] = count        
-    end
-    @h = {}
-    @t = @towels.filter{ pattern.include?(_1)}    
-    countIter(pattern)
-end
-
-p patterns.count{ count(_1) > 0 }
-p patterns.sum{ count(_1) }
+count = patterns.map{count _1}
+p count.count{_1 > 0 }
+p count.sum
