cdcb4f0c1c4be77cb98c7a2c56e9cab3135b293d
thofoer <thofoer@gmx.de>
2024-12-20 19:47:49 +0100

    aoc 2024 Tag 20

--- 2024/20/20.rb
+++ 2024/20/20.rb
@@ -1,21 +1,16 @@
-grid = File.read("input.txt").split("\n")
+start, target, map = nil, nil, Set.new
 
-W, H   = grid.size, grid[0].size
-DIRS   = [1+0i, 0-1i, -1+0i, 0+1i].freeze
-map    = Set.new
-start, target  = nil, nil
-
-W.times do |y|
-  H.times do |x|
-    p = Complex(x,y)
-    value = grid[y][x]
-    map << p unless value == ?#
-    start  = p if value == ?S
-    target = p if value == ?E
-  end
+File.readlines("input.txt").each_with_index do |l, y|
+    l.chars.each.with_index do |c, x| 
+      p = Complex(x,y)     
+      map << p unless c == ?#
+      start  = p if c == ?S
+      target = p if c == ?E
+    end
 end
 
-@dist = Hash.new(Float::INFINITY)
+DIRS  = [1+0i, 0-1i, -1+0i, 0+1i].freeze
+@dist = {}
 
 @dist[target] = 0
 queue = [target]
@@ -26,18 +21,14 @@ until queue.empty?
     visited << node
     DIRS.map{ node + _1 }
         .filter{ map.include?(_1) && !visited.include?(_1)}
-        .each do |pos|            
-            @dist[pos] = @dist[node] + 1
-            queue << pos
-    end    
+        .each{ |pos| @dist[pos] = @dist[node] + 1; queue << pos }    
 end
 
 def manhattan(a, b) = (a.real-b.real).abs + (a.imag-b.imag).abs
 
 def count(cheatsize) =
     @dist.keys.combination(2)
-         .filter{|a,b| manhattan(a,b) <= cheatsize}
-         .count{ |a,b| @dist[b] - @dist[a] >= manhattan(a,b) + 100}
-         #.count(true)
+         .filter{ |a,b| manhattan(a,b) <= cheatsize }
+         .count{ |a,b| @dist[b] - @dist[a] >= manhattan(a,b) + 100 }         
          
 puts count(2), count(20)
