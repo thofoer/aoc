e4f9ec76c48c2c9d0436b8569c87f1bef709ecf9
thofoer <thofoer@gmx.de>
2024-12-23 17:58:54 +0100

    aoc 2024 Tag 23

---
+++ 2024/23/23.rb
@@ -0,0 +1,36 @@
+input = File.read("input.txt").scan(/(..)\-(..)/)
+
+sss = Time.now
+$map = Hash.new {|h,k| h[k] = Set.new}
+
+input.each do |a,b|
+    $map[a]<<b    
+    $map[b]<<a
+end
+
+nets = Set.new
+$map.keys.each do |k|
+    $map[k].to_a
+           .combination(2)
+           .select{ |a,b| $map[a].include?(b)}
+           .each{ |a,b| nets << Set.new([k, a, b])}
+end
+
+puts nets.count{ |c| c.any?{|n| n.start_with?(?t)}}
+
+def findmax(len)
+    puts len
+    $map.each do |k,v|        
+        f = v.to_a
+             .combination(len)
+             .find{ |m| m.combination(2).all?{ |a,b| $map[a].include?(b)}}
+
+        (puts [*f, k].sort.join(?,); return true) if f
+    end
+    false
+end
+
+$map.values.map(&:size).max.downto(1).find{ findmax(_1) }
+
+puts Time.now - sss
+
