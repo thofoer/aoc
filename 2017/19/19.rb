3837df61eefababa99c6504c0d30996f5a78d59e
thofoer <thofoer@gmx.de>
2024-12-31 11:23:25 +0100

    aoc 2017 Tag 19

--- 2017/19/19.rb
+++ 2017/19/19.rb
@@ -3,7 +3,7 @@ pos = nil
 
 File.readlines("input.txt").each_with_index do |l, y|
     l.chars.each.with_index do |c, x|     
-      grid[Complex(x,y)] = c.sub(" ", "")
+      grid[Complex(x,y)] = c.strip
       pos = Complex(x,y) if y == 0 && c==?|
     end
 end
@@ -11,13 +11,12 @@ end
 DIRS = [0+1i, -1+0i, 0-1i, 1+0i]
 dir, steps = 0, 0
 
-loop do
-   # p "#{pos} '#{grid[pos]}' #{dir}"
-   steps += 1
+loop do   
+    steps += 1
     pos += DIRS[dir]   
     print grid[pos] if grid[pos] =~ /[A-Z]/
     break if grid[pos].empty?           
-    dir = (dir + (grid[pos+DIRS[(dir-1)%4]] == "" ? 1 : -1)) % 4 if grid[pos] == ?+           
+    dir = (dir + (grid[pos+DIRS[(dir-1)%4]].empty? ? 1 : -1)) % 4 if grid[pos] == ?+           
 end
 puts
 puts steps
