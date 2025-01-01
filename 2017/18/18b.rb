cfcb52b6abec24556c7cec07085ea3d9e17e256f
thofoer <thofoer@gmx.de>
2025-01-01 14:48:42 +0100

    aoc 2017 Tag 18

--- 2017/18/18b.rb
+++ 2017/18/18b.rb
@@ -1,4 +1,4 @@
-$prg = File.readlines("input.txt").map{|l| l.split}
+$prg = File.readlines("input.txt").map{it.split}
 
 $vars   = [Hash.new(0), Hash.new(0)]
 $ptr    = [0, 0]
@@ -13,7 +13,7 @@ def step(pn)
     val2 = arg2 =~ /[a-z]/ ? $vars[pn][arg2] : arg2.to_i
     
     case op
-        when "snd" then $queues[pn] << val1; $count+=1 if pn==1
+        when "snd" then $queues[pn] << val1; $count += 1 if pn == 1
         when "set" then $vars[pn][arg1] = val2
         when "add" then $vars[pn][arg1] += val2
         when "mul" then $vars[pn][arg1] *= val2
@@ -23,24 +23,16 @@ def step(pn)
             unless $queues[pn ^ 1].empty?
                 $vars[pn][arg1] = $queues[pn ^ 1].shift
             else
-                return :waiting
+                return false
             end
-    end
- 
+    end 
     $ptr[pn] += 1 unless op == "jgz" && val1 > 0
-    return :terminated if $ptr[pn] >= $prg.size
-
-    :busy
+    true
 end
 
 $vars[0][?p] = 0
 $vars[1][?p] = 1
 
-loop do
-    e0, e1 = :busy, :busy
-    e0 = step(0) until e0 == :waiting    
-    e1 = step(1) until e1 == :waiting        
-    break if $queues.all?(&:empty?)
-end
+loop while step(0) || step(1)
 
 p $count
