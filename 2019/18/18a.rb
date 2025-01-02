4aa3677bed3c6c5ccaa3dbc8296b687a55ec0e58
thofoer <thofoer@gmx.de>
2025-01-02 19:54:04 +0100

    aoc 2019 Tag 18

--- 2019/18/18a.rb
+++ 2019/18/18a.rb
@@ -6,33 +6,58 @@ DIRS = [-1+0i, 0-1i, 1+0i, 0+1i]
 grid = Set.new
 doors, keys = {}, {}
 start = nil
+sss = Time.now
+
 File.readlines("input.txt", chomp: true).each_with_index do |l, y|
     l.chars.each.with_index do |c, x| 
         p =  Complex(x,y)
         grid << p unless c == ?#
         start = p if c == ?@
-        doors[p] = c if c =~ /[A-Z]/
+        doors[p] = c.downcase if c =~ /[A-Z]/
         keys[p]  = c if c =~ /[a-z]/
     end
 end
 
+def removeDeadEnds(map, poi=[])
+    ready = false
+    until ready do 
+        ready = true
+        newMap = Set.new
+        map.each do |pos|
+            if (DIRS.map{it + pos}.to_set.intersection(map).size == 1 ) && !poi.include?(pos)            
+                ready = false
+            else
+                newMap << pos 
+            end
+        end
+        map = newMap
+    end
+    map
+end
+
+grid = removeDeadEnds(grid, keys.keys + doors.keys)
 queue = PrioQueue.new
 visited = Set.new
+keybit = Hash.new{ |h,k| h[k] = 1 << (k.bytes.first - 97) }
 
-queue.push [start, Set.new], 0
+allkeys = (1 << keys.size) - 1
+
+queue.push [start, 0], 0
 
 until queue.empty? do
     (curr, kk), dist = queue.pop_with_priority    
-    (puts dist; break) if kk.size == keys.size
+    (puts dist; break) if kk == allkeys
     
     DIRS.map{ it + curr }
-        .select{ grid.include?(it) }        
+        .select{ grid.include?(it) }
+        .reject{ visited.include?([it, kk])}
         .each do |n|        
-            if (doors[n].nil? || kk.include?(doors[n].downcase)) && !visited.include?([n, kk])                
-                newKeys = kk.dup
-                newKeys << keys[n] unless keys[n].nil?
-                visited << [n, newKeys]
-                queue.push [n, newKeys], dist+1 
+            if doors[n].nil? || (kk & keybit[doors[n]]) != 0
+                newState = [n, keys[n].nil? ? kk : kk | keybit[keys[n]]]                
+                visited << newState
+                queue.push newState, dist+1 
             end
         end        
 end    
+
+p Time.now - sss
