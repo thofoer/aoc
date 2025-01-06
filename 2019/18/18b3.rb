2e6efc6aa9789ff5d22e1e6547ef310e841f209b
thofoer <thofoer@gmx.de>
2025-01-06 15:09:13 +0100

    aoc 2019 Tag 18

---
+++ 2019/18/18b3.rb
@@ -0,0 +1,158 @@
+require 'pairing_heap'
+PrioQueue =  PairingHeap::MinPriorityQueue
+State = Struct.new(:pos, :dist, :doors)
+SolveState = Struct.new(:bots, :dist, :keys)
+Key = Struct.new(:pos, :id, :doors)
+Bot = Struct.new(:pos, :env)
+
+DIRS = [-1+0i, 0-1i, 1+0i, 0+1i]
+
+def isKey(c)  = c =~ /[a-z]/
+def isDoor(c) = c =~ /[A-Z]/
+
+sss = Time.now
+$grid = Set.new
+$doors, $keys = {}, {}
+start = nil
+
+File.readlines("sample5.txt", chomp: true).each_with_index do |l, y|
+  l.chars.each.with_index do |c, x|
+    p =  Complex(x,y)
+    $grid << p unless c == ?#
+    start = p if c == ?@
+    $doors[p] = c.downcase if isDoor(c)
+    $keys[p]  = c if isKey(c)
+  end
+end
+
+def makeDistMap
+  distMap = Hash.new{|h,k| h[k] = {} }
+
+  $grid.each do |pos|
+    dists = {}
+    state = State.new(pos, 0)
+    visited = Set.new([pos])
+    queue = [state]
+    until queue.empty?
+      cur = queue.shift
+      DIRS.each do |d|
+        npos = cur.pos + d
+        unless visited.include? npos
+          visited << npos
+          if $grid.include?(npos)
+            dists[npos] = cur.dist + 1
+            queue << State.new(npos, cur.dist + 1)
+          end
+        end
+      end
+    end
+    distMap[pos] = dists
+  end
+  distMap
+end
+
+
+def scanEnvironment(pos)
+  environment = []
+  state = State.new(pos, 0, Set.new)
+  visited = Set.new([pos])
+  queue = [state]
+  until queue.empty?
+    cur = queue.shift
+    DIRS.each do |d|
+      npos = cur.pos + d
+      unless visited.include?(npos)
+        visited << npos
+        newdoors = cur.doors.dup
+        environment << Key.new(npos, $keys[npos], cur.doors) if $keys.include?(npos)
+        newdoors << $doors[npos].downcase if $doors.include?(npos)
+        queue << State.new(npos, cur.dist + 1, newdoors) if $grid.include?(npos)
+      end
+    end
+  end
+  environment
+end
+
+def makeBotId(bots, keys)
+  bots.map{|x,y| "r#{x}c#{y}"}.join + keys.to_a.sort.join
+end
+bots = []
+bots << start + Complex(-1, -1)
+bots << start + Complex(1, -1)
+bots << start + Complex(1, 1)
+bots << start + Complex(-1, 1)
+
+
+botkeys = [
+  scanEnvironment( bots[0]),
+  scanEnvironment( bots[1]),
+  scanEnvironment( bots[2]),
+  scanEnvironment( bots[3])
+]
+p botkeys
+
+p Time.now - sss
+
+dists = makeDistMap
+solved = false
+start = { :b => bots.map(&:dup), :d => 0, :k => Set.new, :id =>  makeBotId(bots, Set.new)}
+seen = {}
+seen[start[:id]] = 0
+
+queue = PrioQueue.new
+queue.push start
+
+until queue.empty? || solved
+  cur = queue.pop
+  puts "CURRENT #{cur.inspect}"
+  next if seen[cur[:id]] < cur[:d]
+  (0..3).each do |i|
+    puts "BOT = #{cur[:b][i].inspect}"
+    botkeys[i].each do |key|
+      puts "KEY = #{key.inspect}"
+
+      if !cur[:k].include? key.id
+        p "#{cur[:k]}   #{key.id}"
+        p "r#{cur[:b][i].imag}c#{cur[:b][i].real}", "r#{key.pos.imag}c#{key.pos.real}"
+        #gets
+        reachable = true
+        key[:doors].each do |door|
+          reachable = false unless cur[:k].include? door
+        end
+        if reachable
+
+          puts "============================"
+
+          puts "current bot: #{cur[:b][i]}  #{i}"
+          puts "current key: #{key.pos}"
+          p dists
+
+          dist = dists[cur[:b][i]][key.pos]
+          p dist
+          #  gets
+
+          nd = cur[:d] + dist
+          nk = cur[:k].dup
+          nk << key.id
+          if nk.size == $keys.size
+            solution = nd
+            solved = true
+            puts solution
+            break
+          end
+          nb = cur[:b].map(&:dup)
+          nb[i] = key.pos
+          nid = makeBotId(nb, nk)
+          if !seen[nid] || seen[nid] > nd
+            #p "PUSH #{{:b=>nb, :d=>nd, :k=>nk, :id=>nid}}, #{nd}"
+            queue.push({:b=>nb, :d=>nd, :k=>nk, :id=>nid}, nd)
+            seen[nid] = nd
+          end
+        end
+      end
+    end
+  end
+end
+
+
+p Time.now - sss
