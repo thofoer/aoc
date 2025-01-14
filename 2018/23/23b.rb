50444685e91a5d669ef1033eda0d8062c42ecdc1
thofoer <thofoer@gmx.de>
2025-01-14 12:56:48 +0100

    aoc 2018 Tag 23

--- 2018/23/23b.rb
+++ 2018/23/23b.rb
@@ -1,31 +1,29 @@
 require "z3"
-bots = File.read("input.txt").scan(/(-?\d+),(-?\d+),(-?\d+).*?(\d+)/).map{ it.map(&:to_i)}
+bots = File.read("input.txt").scan(/(-?\d+),(-?\d+),(-?\d+).*?(\d+)/).map{ _1.map(&:to_i)}
 
 x, y, z = Z3.Int(?x), Z3.Int(?y), Z3.Int(?z)
 
-botRanges = bots.size.times.to_a.map{ Z3.Int("range#{it}") }
+botRanges = bots.size.times.to_a.map{ Z3.Int("range#{_1}") }
 
 optimizer = Z3::Optimize.new
 
-def z3Abs(n) = Z3.IfThenElse(n > 0, n, -n)
-def manhattan(x, y, z) = z3Abs(x) + z3Abs(y) + z3Abs(z) 
+def manhattan(x, y, z) = x.abs + y.abs + z.abs
 
 botRanges.each.with_index do |botrange, i|    
     bx, by, bz, range = bots[i]
     optimizer.assert( botrange == Z3.IfThenElse( manhattan(x - bx, y - by, z - bz) <= range, 1, 0))    
 end
 
-rangeCount   = Z3.Int "sum"
-distFromZero = Z3.Int "dist"
+rangeCount = Z3.Int "sum"
+distance   = Z3.Int "dist"
 
-optimizer.assert(rangeCount   == Z3.Add(*botRanges) )
-optimizer.assert(distFromZero == z3Abs(x) + z3Abs(y) + z3Abs(z))
+optimizer.assert(rangeCount == Z3.Add(*botRanges) )
+optimizer.assert(distance   == manhattan(x, y, z))
 
 optimizer.maximize(rangeCount)
-optimizer.minimize(distFromZero)
-#optimizer.maximize(distFromZero)
+optimizer.minimize(distance)
+#optimizer.maximize(distance)
 optimizer.check()
 #p optimizer.model
 
-p [x, y, z].sum{optimizer.model[it].to_i}
-#p optimizer.model[x].to_i+ optimizer.model[y].to_i+ optimizer.model[z].to_i
+p optimizer.model.model_eval(x + y + z).to_i
