d7fa6b063c876dde72a0b5f219f55d03cf94ef93
thofoer <thofoer@gmx.de>
2025-01-14 12:55:59 +0100

    aoc 2020 Tag 01

--- 2020/01/01.rb
+++ 2020/01/01.rb
@@ -1,4 +1,4 @@
-input = File.read("input.txt").split("\n").map(&:to_i)
+input = File.readlines("input.txt").map(&:to_i)
 
-puts input.combination(2).select{ |a, b| a + b == 2020}.map{|a,b| a*b }.first
-puts input.combination(3).select{ |a, b, c| a + b + c == 2020}.map{|a, b, c| a * b * c }.first
+p input.combination(2).find{ it.sum == 2020}.reduce(&:*)
+p input.combination(3).find{ it.sum == 2020}.reduce(&:*)
