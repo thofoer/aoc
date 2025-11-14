65d311edfb2e208276d0321a2c4d13d8fd878605
thofoer <thofoer@gmx.de>
2025-11-14 16:32:59 +0100

    aoc 2015 Tag 19

--- 2015/19/19b.rb
+++ 2015/19/19b.rb
@@ -1,48 +1,12 @@
-require 'pairing_heap'
-PrioQueue =  PairingHeap::MinPriorityQueue
+input = File.read("input.txt", chomp: true).split("\n\n")
+rules = input[0].split("\n").map { it.scan(/(\w+) => (\w+)/)}.flatten(1).map{|k,v| [v.reverse, k.reverse]}.to_h
+target = input[1].reverse
+regex = Regexp.new rules.keys.join("|")
 
-input = File.read("input.txt").split("\n\n")
-RULES = input[0].split("\n").map { it.scan(/(\w+) => (\w+)/)}.flatten(1).map{|a,b| [b,a]}.group_by(&:first).map{|k,v| [k, v.map(&:last)]}.to_h
-target = input[1]
-
-def step(molecule)
-    #puts "----> #{molecule.inspect}"
-    res = Set.new
-    RULES.each do |k,v|
-        indices = molecule.enum_for(:scan, Regexp.new("(#{k})")).map do
-            Regexp.last_match.offset(0).first
-        end    
-        l = k.length
-        v.each do |r|        
-            indices.each do |i|
-                m = molecule.dup
-                m[i..(i+l-1)] = r
-                res << m
-            end
-        end    
-    end
-    res
+z = 0
+while target != ?e
+    range = target.match(regex).offset(0)
+    target[range.first...range.last] = rules[$&]
+    z = z + 1    
 end
-
-queue = PrioQueue.new
-#target="HOHOHO"
-queue.push [target, 0], target.length
-
-seen = Set.new
-
-while !queue.empty? do
-    str, steps = queue.pop
-    if str == ?e
-        puts steps
-        return
-    end
-    next if seen.include?(str)
-    puts "#{steps} #{seen.size}  #{str}" if seen.size % 10000 == 0
-    seen << str
-    step(str).each do |n|
-        state = [n, steps + 1]
-        queue.push state, n.length * 1000 + steps unless queue.include?(state)
-    end
-
-
-end
+print z
