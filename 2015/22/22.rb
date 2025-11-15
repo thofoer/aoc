576f45c5bb0efe30e3d1db804de2b91b8f702581
thofoer <thofoer@gmx.de>
2025-11-15 17:29:36 +0100

    2015 Tag 22

---
+++ 2015/22/22.rb
@@ -0,0 +1,69 @@
+BOSSLIFE, DAMAGE = File.readlines("input.txt").map{ it.scan(/(\d+)/)}.flatten.map(&:to_i)
+
+class State
+    attr_reader :playerturn, :mana, :hp, :poison, :shield, :recharge, :spent, :boss    
+
+    def initialize(playerturn=true, mana=500, hp=50, poison=0, shield=0, recharge=0, spent=0, boss=BOSSLIFE)
+        @playerturn, @mana, @hp, @poison, @shield, @recharge, @spent, @boss = playerturn, mana, hp, poison, shield, recharge, spent, boss        
+    end
+
+    def lose? = @mana < 53 || @hp <= 0    
+    def win?  = @boss <= 0
+    
+    def magicmissile
+        State.new(false, @mana - 53, @hp, @poison, @shield, @recharge, @spent + 53, @boss - 4) if @mana >= 53
+    end
+
+    def drain
+        State.new(false, @mana - 73, @hp + 2, @poison, @shield, @recharge, @spent + 73, @boss - 2) if @mana >= 73
+    end
+
+    def shield
+        State.new(false, @mana - 113, @hp, @poison, 6, @recharge, @spent + 113, @boss) if @mana >= 113 && @shield <= 0
+    end
+
+    def poison
+        State.new(false, @mana - 173, @hp, 6, @shield, @recharge, @spent + 173, @boss) if @mana >= 173 && @poison <= 0
+    end
+
+    def recharge
+        State.new(false, @mana - 229, @hp, @poison, @shield, 5, @spent + 229, @boss) if @mana >= 229 && @recharge <= 0
+    end
+
+    def turn(hard)        
+        @boss -= 3 if @poison > 0
+        @mana += 101 if @recharge > 0
+        @poison -= 1
+        @recharge -= 1
+        @shield -= 1
+        
+        if @playerturn
+            @hp -= hard ? 1 : 0
+            [magicmissile, drain, shield, poison, recharge].compact
+        else
+            damage = DAMAGE - (@shield > 0 ? 7 : 0)
+            [State.new(true, @mana, @hp - damage, @poison, @shield, @recharge, @spent, @boss)]
+        end
+    end
+end
+
+def solve(hard = false)
+    queue = []
+    best = 100000
+    queue.push State.new()
+
+    until queue.empty? do
+        s = queue.pop    
+        next if s.spent > best
+        
+        if s.win?
+            best = [best, s.spent].min             
+        elsif !s.lose?        
+            s.turn(hard).each{ queue.push it }
+        end
+    end
+    best
+end
+
+p solve
+p solve(true)
