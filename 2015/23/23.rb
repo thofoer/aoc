59c1982b0a11a7405a201d843aa4c3c82d4d2d62
thofoer <thofoer@gmx.de>
2025-11-15 18:17:48 +0100

    2015 Tag 23

---
+++ 2015/23/23.rb
@@ -0,0 +1,23 @@
+PRG = File.readlines("input.txt").map{ it.scan(/(\w+) ([ab+\-0-9]+),? ?(.*)/)}.flatten(1)
+
+def solve(a)
+    z = 0
+    reg = {?a => a, ?b => 0}
+
+    while z < PRG.size
+        c, r, o = PRG[z]
+
+        case c
+        when "hlf" then reg[r] /= 2; 
+        when "tpl" then reg[r] *= 3; 
+        when "inc" then reg[r] += 1;
+        when "jmp" then z += r.to_i - 1
+        when "jie" then z += o.to_i - 1 if reg[r].even?
+        when "jio" then z += o.to_i - 1 if reg[r] == 1
+        end
+        z += 1
+    end
+    reg[?b]
+end
+
+p solve(0), solve(1)
