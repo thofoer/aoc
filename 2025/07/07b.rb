2557204b306c11ab8dfb5c066fd8227a91d81b2f
thofoer <thofoer@gmx.de>
2025-12-07 16:41:03 +0100

    2025 - Tag 7

--- 2025/07/07b.rb
+++ 2025/07/07b.rb
@@ -4,12 +4,12 @@ MEM = {}
 
 def solve(pos) = 
     MEM[pos] ||= case
-                when pos.imag == GRID.size - 1 
-                    1
-                when GRID[pos.imag+1][pos.real] == ?^
-                    solve(pos + 1+1i) + solve(pos - 1+1i)
-                else
-                    solve(pos + 0+1i)        
+                    when pos.imag == GRID.size - 1 
+                        1
+                    when GRID[pos.imag+1][pos.real] == ?^
+                        solve(pos + 1+1i) + solve(pos - 1+1i)
+                    else
+                        solve(pos + 0+1i)        
                 end        
 
 puts solve(start)
