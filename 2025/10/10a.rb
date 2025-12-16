37f650bfb9249596acb057270733f722902883a3
thofoer <thofoer@gmx.de>
2025-12-16 19:45:11 +0100

    2025 - Tag 10

--- 2025/10/10a.rb
+++ 2025/10/10a.rb
@@ -1,14 +1,11 @@
 input = File.readlines("input.txt")
-            .map{ it.scan(/\[(.+)\] ([()\d, ]+) {(.+)}/)}
+            .map{ it.scan(/\[(.+)\] ([()\d, ]+)/)}
             .flatten(1)
-            .map{ |l,b,j| 
+            .map{ |l,b| 
                         [
                             l.reverse.gsub(?.,?0).gsub(?#,?1).to_i(2), 
                             b.scan(/\((.+?)\)/).flatten.map{ it.split(?,).map{1 << it.to_i}.reduce(&:|)}
                         ]
             }
 
-z = input[2][0]
-b = input[2][1]
-
-p input.sum {|z,b| 0.upto(b.size-1).find{|c| b.combination(c).any?{ it.reduce(&:^) == z}} }
+puts input.sum {|z,b| 1.upto(b.size).find{|c| b.combination(c).any?{ it.reduce(&:^) == z}} }
